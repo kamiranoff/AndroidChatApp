f19cfec760b6e874c86f20b460c13d07d04d4f5a
Kevin Amiranoff <kamiranoff@gmail.com>
2018-11-24 19:58:18 +0000

    Adds random avatar generator

--- app/src/main/java/com/nemeantalestudios/androidchatapp/SignupActivity.kt
+++ app/src/main/java/com/nemeantalestudios/androidchatapp/SignupActivity.kt
@@ -1,10 +1,15 @@
 package com.nemeantalestudios.androidchatapp
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_signup.*
+import java.util.*
 
 class SignupActivity : AppCompatActivity() {
+
+    var avatarColor = "[0.5,0.5,0.5,1]"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,11 +17,38 @@ class SignupActivity : AppCompatActivity() {
     }
 
     fun onChooseAvatarClicked(view: View) {
+        val random = Random()
+        val avatarNumber = random.nextInt(100) + 1
+        val avatarName = "heroes$avatarNumber"
+        val resourceId = resources.getIdentifier(avatarName, "drawable", packageName)
+        chooseAvatarImage.setImageResource(resourceId);
+    }
+
+    fun convertToIOSColor(r: Int, g: Int, b: Int): String {
+
+
+        val savedR = r.toDouble() / 255
+        val savedG = g.toDouble() / 255
+        val savedB = b.toDouble() / 255
+
+        avatarColor = "[$savedR, $savedG, $savedB, 1]"
+
+        return avatarColor;
 
     }
 
-    fun onGenerateBgColorClicked(view: View) {
+    fun onRemoveBgColor(view: View) {
+        chooseAvatarImage.setBackgroundColor(0x00000000)
+    }
 
+    fun onGenerateBgColorClicked(view: View) {
+        val random = Random()
+        val r = random.nextInt(256)
+        val g = random.nextInt(256)
+        val b = random.nextInt(256)
+
+        chooseAvatarImage.setBackgroundColor(Color.rgb(r, g, b))
+        convertToIOSColor(r, g, b);
     }
 
 

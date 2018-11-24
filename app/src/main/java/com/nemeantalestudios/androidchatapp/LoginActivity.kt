add8341692e7d556e54d18f4845e3f1157bfeb79
Kevin Amiranoff <kamiranoff@gmail.com>
2018-11-24 19:13:28 +0000

    adds signup activity

--- app/src/main/java/com/nemeantalestudios/androidchatapp/LoginActivity.kt
+++ app/src/main/java/com/nemeantalestudios/androidchatapp/LoginActivity.kt
@@ -1,18 +1,22 @@
 package com.nemeantalestudios.androidchatapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
 class LoginActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
     }
 
     fun loginCreateUserBtnClicked(view: View) {
-
+        val signupActivity = Intent(this, SignupActivity::class.java)
+        startActivity(signupActivity)
 
     }
 
---
+++ app/src/main/java/com/nemeantalestudios/androidchatapp/SignupActivity.kt
@@ -0,0 +1,27 @@
+package com.nemeantalestudios.androidchatapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class SignupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+
+    fun onChooseAvatarClicked(view: View) {
+
+    }
+
+    fun onGenerateBgColorClicked(view: View) {
+
+    }
+
+
+    fun onSignupUserClicked(view: View) {
+
+    }
+
+}
